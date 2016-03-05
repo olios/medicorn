9533eb52a6a384c5557f6c52ccb0e81057e06eaf
Tomas Trnka <tomas@trnkatomas.eu>
2016-03-05 18:34:24 +0100

    desing changes

--- emCase/device/models.py
+++ emCase/device/models.py
@@ -19,7 +19,7 @@ class Device(models.Model):
             )
 
     id_number = models.IntegerField(unique=True, verbose_name="Serial Number of IoT")
-    human_readable_name = models.TextField(verbose_name="Human readable name")
+    human_readable_name = models.CharField(max_length=100, verbose_name="Human readable name")
     insert_date = models.DateTimeField(verbose_name="Insert Date")
     type_of_device = models.CharField(max_length=100, choices=type_choice, verbose_name="Type of a device")
     last_check_date = models.DateTimeField(verbose_name="Last Check date")
@@ -28,5 +28,7 @@ class Device(models.Model):
     description_of_content = models.TextField(verbose_name = "Description of a content")
     status = models.CharField(max_length=100, choices=status_choice, verbose_name = "Status")
     posted = models.BooleanField(default=False, verbose_name="Posted")
+    ip_addrr = models.GenericIPAddressField(blank=False, verbose_name="IP addrres")
+    port = models.IntegerField(verbose_name="Port")
 
     
