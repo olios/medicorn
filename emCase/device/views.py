38ef889f815db1dff4d263962b720aada3c2a26f
kotrfa <kotrfa@gmail.com>
2016-03-05 12:30:01 +0100

    base html and views

--- emCase/device/views.py
+++ emCase/device/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the device index.")
+    return render(request, 'index.html', {'devices' : Device.objects.all()})
 
-
+# def device(request):
+#    
