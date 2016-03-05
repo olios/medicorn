9a91856dc61bace80a6aa29c45ae36be9de43bc7
kotrfa <kotrfa@gmail.com>
2016-03-05 20:01:39 +0100

    form

--- emCase/location/views.py
+++ emCase/location/views.py
@@ -4,10 +4,27 @@ from django.shortcuts import render
 
 from location.models import Location
 from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+
+from .forms import LocationForm
 
 # Create your views here.
 def index_location(request):
     return render(request, 'index_location.html', { })
+
+from django.views.decorators.csrf import csrf_exempt
+@csrf_exempt
+def run_location(request):
+    if request.method == "POST":
+        form = LocationForm(request.POST)
+        
+        if form.is_valid():
+            # RUN devices!
+            # from run_devices import ...
+            return HttpResponseRedirect('/devs_activated')
+    else:
+        form = LocationForm()
+    return render(request, 'loc_form.html', {'form' : form})
 
 """def view_device(request, id_number):
 	#id_number = request.GET.get("id_number")
