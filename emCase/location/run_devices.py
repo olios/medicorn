427989047f9772304c501bc574ddf529febc0904
Tomas Trnka <tomas@trnkatomas.eu>
2016-03-05 23:06:14 +0100

    sobota final sync

--- emCase/location/run_devices.py
+++ emCase/location/run_devices.py
@@ -5,7 +5,7 @@ def get_nearby_devices(lng, lat, type_of_dev, radius=500):
     # TODO
 
     from device.models import Device
-    return Device.objects.all()
+    return Device.objects.filter(type_of_device=type_of_dev)
 
 
 def toggle_devices(list_of_devs, command):
@@ -18,11 +18,11 @@ def toggle_devices(list_of_devs, command):
         except:
             print("NEJAKA CHYBA!")
             chk = False
-            raise
-        check_list[device.id] = chk
+        check_list[device.human_readable_name] = chk
     return check_list
 
 def megafunc(lng, lat, command):
-    devices = get_nearby_devices(lng, lat)
-    d = toggle_devices(devices, command)
+    cmd, typ = command.split()
+    devices = get_nearby_devices(lng, lat, typ)
+    d = toggle_devices(devices, cmd)
     return d
