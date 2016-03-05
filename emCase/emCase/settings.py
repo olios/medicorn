d75280ea7e8c3df0baf9ffc9f0458591e1f19530
kotrfa <kotrfa@gmail.com>
2016-03-05 13:12:12 +0100

    cleaning and adding static files

--- emCase/emCase/settings.py
+++ emCase/emCase/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'emCase.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.core.context_processors.static',
             ],
         },
     },
