821757892d146969ba6b144d24ba71939bc5bead
kotrfa <kotrfa@gmail.com>
2016-03-05 22:28:08 +0100

    stop

--- emCase/location/run_devices.py
+++ emCase/location/run_devices.py
@@ -13,11 +13,12 @@ def toggle_devices(list_of_devs, command):
     chk = False
     for device in list_of_devs:
         try:
-            urlopen("http://{}:{}/{}".format(device.ip_addr, device.port))
+            urlopen("http://{}:{}/{}".format(device.ip_addrr, device.port, command))
             chk = True
         except:
             print("NEJAKA CHYBA!")
             chk = False
+            raise
         check_list[device.id] = chk
     return check_list
 
