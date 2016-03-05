3366a67ade89e8fa947ff90f02782cc2ce5bc45d
Tomas Trnka <tomas@trnkatomas.eu>
2016-03-05 16:53:43 +0100

    Locations

--- emCase/emCase/urls.py
+++ emCase/emCase/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', include('location.urls')),
     url(r'^device/', include('device.urls')),
 ]
