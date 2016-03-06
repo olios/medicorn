41375c2cef5fafcbbd8d3e2a969e6a3950c38ceb
Tomas Trnka <tomas@trnkatomas.eu>
2016-03-06 10:02:07 +0100

    mapa s markery

--- emCase/device/urls.py
+++ emCase/device/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^visual$', views.visualization, name='visualization'),
+    #url(r'^visual$', views.visualization, name='visualization'),
     url(r'^view_device/(?P<id_number>\d+)$', views.view_device, name='view_device'),
     url(r'^$', views.index, name='index'),    
 ]
